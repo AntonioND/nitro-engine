01f57b8609472c5b046a456067fd1cc71f494ef3
Antonio Niño Díaz <antonio_nd@outlook.com>
2019-06-04 18:08:18 +0100

    Fix coding style of remaining effects examples

--- examples/effects/screen_effects/source/main.c
+++ examples/effects/screen_effects/source/main.c
@@ -1,14 +1,21 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2008-2011, 2019, Antonio Niño Díaz
+//
+// This file is part of Nitro Engine
 
 #include <NEMain.h>
+
 #include "nitrocat_bin.h"
 
-NE_Camera * Camera; //Create pointers to objects...
-NE_Model * Model;
-
+// Create pointers to objects...
+NE_Camera *Camera;
+NE_Model *Model;
 
 void Draw3DScene(void)
 {
-	NE_ClearColorSet(NE_Red, 31,63); //Set rear plane color
+	// Set rear plane color
+	NE_ClearColorSet(NE_Red, 31, 63);
 
 	NE_CameraUse(Camera);
 	NE_ModelDraw(Model);
@@ -16,60 +23,86 @@ void Draw3DScene(void)
 
 void Draw3DScene2(void)
 {
-	NE_ClearColorSet(NE_Green, 31,63); //Set rear plane color
+	// Set rear plane color
+	NE_ClearColorSet(NE_Green, 31, 63);
 
 	NE_CameraUse(Camera);
 	NE_ModelDraw(Model);
 }
 
-int main()
+int main(void)
 {
+	// This is needed for special screen effects
 	irqEnable(IRQ_HBLANK);
-	irqSet(IRQ_VBLANK, NE_VBLFunc); //Needed for special effects
+	irqSet(IRQ_VBLANK, NE_VBLFunc);
 	irqSet(IRQ_HBLANK, NE_HBLFunc);
-	
-	NE_InitDual3D();  //Init dual 3D mode and console
-	NE_InitConsole();
-	
-	Model = NE_ModelCreate(NE_Static); //Create space for objects...
-	Camera = NE_CameraCreate();
-	
-	NE_CameraSet(Camera, -8,0,0, 0,0,0, 0,1,0); //Set camera
-	
-	NE_ModelLoadStaticMesh(Model,(u32*)nitrocat_bin); //Load model...
-	
-	NE_LightSet(0,NE_White,-0.5,-0.5,-0.5); //Set light color and direction
-	
-	NE_ShadingEnable(true); //Enable shading...
-	
-	//NE_SpecialEffectNoiseConfig(31);
 
-	//NE_SpecialEffectSineConfig(3,8);
-	
-	while(1) 
-	{
+	// Init dual 3D mode and console
+	NE_InitDual3D();
+	NE_InitConsole();
+
+	// Allocate objects...
+	Model = NE_ModelCreate(NE_Static);
+	Camera = NE_CameraCreate();
+
+	// Setup camera
+	NE_CameraSet(Camera,
+		     -8, 0, 0,
+		     0, 0, 0,
+		     0, 1, 0);
+
+	// Load model...
+	NE_ModelLoadStaticMesh(Model, (u32 *)nitrocat_bin);
+
+	// Set light color and direction
+	NE_LightSet(0,NE_White,-0.5,-0.5,-0.5);
+
+	// Enable shading...
+	NE_ShadingEnable(true);
+
+	// Other test configurations
+	//NE_SpecialEffectNoiseConfig(31);
+	//NE_SpecialEffectSineConfig(3, 8);
+
+	while (1) {
+		// Get keys information
 		scanKeys();
-		int keys = keysHeld(); //Get keys information
+		int keys = keysHeld();
 		int kdown = keysDown();
-		
-		printf("\x1b[0;0HPad: Rotate.\nA: Sine effect.\nB: Noise effect.\nX: Deactivate effects.");
-		printf("\nL/R: Pause/Unpause.");
-		
-		if(keys & KEY_UP) NE_ModelRotate(Model, 0, 0, 2);
-		if(keys & KEY_DOWN) NE_ModelRotate(Model, 0, 0, -2); //Rotate model
-		if(keys & KEY_RIGHT) NE_ModelRotate(Model, 0, 2, 0);
-		if(keys & KEY_LEFT) NE_ModelRotate(Model, 0, -2, 0);
-		
-		if(kdown & KEY_B) NE_SpecialEffectSet(NE_NOISE); //Activate effects
-		if(kdown & KEY_A) NE_SpecialEffectSet(NE_SINE);
-		if(kdown & KEY_X) NE_SpecialEffectSet(0);	 //Deactivate effects
-		
-		if(kdown & KEY_L) NE_SpecialEffectPause(true);
-		if(kdown & KEY_R) NE_SpecialEffectPause(false);
-		
-		NE_ProcessDual(Draw3DScene, Draw3DScene2); //Draw 3D scene
+
+		printf("\x1b[0;0H"
+		       "Pad: Rotate.\nA: Sine effect.\nB: Noise effect.\n"
+		       "X: Deactivate effects.\nL/R: Pause/Unpause.");
+
+		// Rotate model
+		if (keys & KEY_UP)
+			NE_ModelRotate(Model, 0, 0, 2);
+		if (keys & KEY_DOWN)
+			NE_ModelRotate(Model, 0, 0, -2);
+		if (keys & KEY_RIGHT)
+			NE_ModelRotate(Model, 0, 2, 0);
+		if (keys & KEY_LEFT)
+			NE_ModelRotate(Model, 0, -2, 0);
+
+		// Activate effects
+		if (kdown & KEY_B)
+			NE_SpecialEffectSet(NE_NOISE);
+		if (kdown & KEY_A)
+			NE_SpecialEffectSet(NE_SINE);
+		// Deactivate effects
+		if (kdown & KEY_X)
+			NE_SpecialEffectSet(0);
+
+		// Pause effects
+		if (kdown & KEY_L)
+			NE_SpecialEffectPause(true);
+		if (kdown & KEY_R)
+			NE_SpecialEffectPause(false);
+
+		// Draw 3D scenes
+		NE_ProcessDual(Draw3DScene, Draw3DScene2);
 		NE_WaitForVBL(0);
 	}
-	
+
 	return 0;
 }
--- examples/effects/shading_alpha_outlining/source/main.c
+++ examples/effects/shading_alpha_outlining/source/main.c
@@ -1,87 +1,131 @@
+// SPDX-License-Identifier: MIT
+//
+// Copyright (c) 2008-2011, 2019, Antonio Niño Díaz
+//
+// This file is part of Nitro Engine
 
 #include <NEMain.h>
+
 #include "nitrocat_bin.h"
 #include "texture_bin.h"
 
-NE_Camera * Camera;     //We use pointers to use less ram if we are not using 3D, but you can 
-NE_Model * Model;       //create the structs directly and you won't have to use Create/Delete
-NE_Material * Material; //functions.
+// We use pointers to use less ram if we are not using 3D, but you can create
+// the structs directly and you won't have to use Create/Delete functions.
+NE_Camera *Camera;
+NE_Model *Model;
+NE_Material *Material;
 
 int shading, alpha, id;
 
 void Draw3DScene(void)
 {
-	NE_CameraUse(Camera);   //Set camera
+	//Set camera
+	NE_CameraUse(Camera);
 
-	NE_PolyFormat(alpha,id,NE_LIGHT_0,NE_CULL_BACK,shading);	//Set polygon format
+	//Set polygon format
+	NE_PolyFormat(alpha, id, NE_LIGHT_0, NE_CULL_BACK, shading);
 
-	NE_ModelDraw(Model); //Draw model...
+	//Draw model...
+	NE_ModelDraw(Model);
 }
 
-int main()
+int main(void)
 {
+	// This is needed for special screen effects
 	irqEnable(IRQ_HBLANK);
-	irqSet(IRQ_VBLANK, NE_VBLFunc); //This is needed for special screen effects
+	irqSet(IRQ_VBLANK, NE_VBLFunc);
 	irqSet(IRQ_VBLANK, NE_HBLFunc);
-	
-	NE_Init3D();  //Init Nitro Engine in normal 3D mode
-	NE_TextureSystemReset(0,0,NE_VRAM_AB); // libnds uses VRAM_C for the text console
-	consoleDemoInit(); //This is needed to print text
 
-	Model = NE_ModelCreate(NE_Static);  //Create space for the things we will use
+	// Init console and Nitro Engine
+	NE_Init3D();
+	// libnds uses VRAM_C for the demo text console
+	NE_TextureSystemReset(0, 0, NE_VRAM_AB);
+	// This is needed to print text
+	consoleDemoInit();
+
+	// Allocate the things we will use
+	Model = NE_ModelCreate(NE_Static);
 	Camera = NE_CameraCreate();
 	Material = NE_MaterialCreate();
-	
-	NE_CameraSet(Camera, -8,0,0, 0,0,0, 0,1,0); //Set coordinates for the camera
-	
-	//Load mesh from RAM and assign it to the object "Model".
-	NE_ModelLoadStaticMesh(Model,(u32*)nitrocat_bin);
-	//Load texture from RAM and assign it to "Material".
-	NE_MaterialTexLoad(Material, GL_RGB, 128, 128, TEXGEN_TEXCOORD, (u8*) texture_bin);
-	//Assign texture to model...
+
+	// Set coordinates for the camera
+	NE_CameraSet(Camera,
+		     -8, 0, 0,
+		     0, 0, 0,
+		     0, 1, 0);
+
+	// Load mesh from RAM and assign it to the object "Model".
+	NE_ModelLoadStaticMesh(Model, (u32 *)nitrocat_bin);
+	// Load texture from RAM and assign it to "Material".
+	NE_MaterialTexLoad(Material, GL_RGB, 128, 128, TEXGEN_TEXCOORD,
+			   (u8 *) texture_bin);
+
+	// Assign texture to model...
 	NE_ModelSetMaterial(Model, Material);
-	
-	NE_MaterialSetPropierties(Material, //Set some propierties to Material
-						RGB15(24,24,24), //diffuse
-						RGB15(8,8,8), //ambient
-						RGB15(0,0,0), //specular
-						RGB15(0,0,0), //emission
-						false, false); //vtxcolor, useshininess);
 
-	NE_LightSet(0,NE_White,-0.5,-0.5,-0.5); //We set a light and its color
-	
-	NE_ShadingEnable(true); //This enables shading (you can choose normal or toon).
-	NE_OutliningEnable(true); //This enables outlining in all polygons, so be careful
+	//Set some propierties to Material
+	NE_MaterialSetPropierties(Material,
+				  RGB15(24,24,24), //diffuse
+				  RGB15(8,8,8),    // ambient
+				  RGB15(0,0,0),    // specular
+				  RGB15(0,0,0),    // emission
+				  false, false);   // vtxcolor, useshininess
 
-	//We set the second outlining color to red.
-	//This will be used by polygons with ID 8 - 15.
-	NE_OutliningSetColor(1, NE_Red);      
-	
-	while(1) 
-	{
-		scanKeys();  //Get keys information
+	// We set a light and its color
+	NE_LightSet(0, NE_White, -0.5, -0.5, -0.5);
+
+	// This enables shading (you can choose normal or toon).
+	NE_ShadingEnable(true);
+	// This enables outlining in all polygons, so be careful
+	NE_OutliningEnable(true);
+
+	// We set the second outlining color to red.
+	// This will be used by polygons with ID 8 - 15.
+	NE_OutliningSetColor(1, NE_Red);
+
+	while(1) {
+		// Get keys information
+		scanKeys();
 		int keys = keysHeld();
-		
-		printf("\x1b[0;0HPad: Rotate.\nA: Toon shading.\nB: Change alpha value.\nY: Wireframe mode (alpha = 0)\nX: Outlining.");
-		
-		if(keys & KEY_UP) NE_ModelRotate(Model, 0, 0, 2);  //Rotate model using the pad
-		if(keys & KEY_DOWN) NE_ModelRotate(Model, 0, 0, -2);
-		if(keys & KEY_RIGHT) NE_ModelRotate(Model, 0, 2, 0);
-		if(keys & KEY_LEFT) NE_ModelRotate(Model, 0, -2, 0);
-		
-		if(keys & KEY_A) shading = NE_TOON_SHADING; //Change shading type
-		else shading = NE_MODULATION;
-		
-		if(keys & KEY_B) alpha = 15;  // Transparent
-		else if(keys & KEY_Y) alpha = 0;  //Wireframe
-		else alpha = 31; //Opaque
-		
-		if(keys & KEY_X) id = 8; //Change id co change outlining color
-		else id = 0;
-		
-		NE_Process(Draw3DScene); //Draws scene
+
+		printf("\x1b[0;0H"
+		       "Pad: Rotate.\nA: Toon shading.\n"
+		       "B: Change alpha value.\nY: Wireframe mode (alpha = 0)\n"
+		       "X: Outlining.");
+
+		// Rotate model using the pad
+		if (keys & KEY_UP)
+			NE_ModelRotate(Model, 0, 0, 2);
+		if (keys & KEY_DOWN)
+			NE_ModelRotate(Model, 0, 0, -2);
+		if (keys & KEY_RIGHT)
+			NE_ModelRotate(Model, 0, 2, 0);
+		if (keys & KEY_LEFT)
+			NE_ModelRotate(Model, 0, -2, 0);
+
+		// Change shading type
+		if (keys & KEY_A)
+			shading = NE_TOON_SHADING;
+		else
+			shading = NE_MODULATION;
+
+		if (keys & KEY_B)
+			alpha = 15; // Transparent
+		else if (keys & KEY_Y)
+			alpha = 0;  // Wireframe
+		else
+			alpha = 31; // Opaque
+
+		// Change id co change outlining color
+		if (keys & KEY_X)
+			id = 8;
+		else
+			id = 0;
+
+		// Draw scene
+		NE_Process(Draw3DScene);
 		NE_WaitForVBL(0);
 	}
-	
+
 	return 0;
 }
