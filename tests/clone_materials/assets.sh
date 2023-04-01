d044d8c19fdcb70fc20f69d76f21dde2a8d70717
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-04-01 14:55:07 +0100

    tests: Fix assets script

--- tests/clone_materials/assets.sh
+++ tests/clone_materials/assets.sh
@@ -5,13 +5,7 @@ TOOLS=$NITRO_ENGINE/tools
 IMG2DS=$TOOLS/img2ds/img2ds.py
 
 python3 $IMG2DS \
-    --input assets/a3pal32.png \
+    --input $NITRO_ENGINE/examples/assets/a3pal32.png \
     --name a3pal32 \
     --output data \
     --format A3PAL32
-
-python3 $IMG2DS \
-    --input assets/pal4.png \
-    --name pal4 \
-    --output data \
-    --format PAL4
