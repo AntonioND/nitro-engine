2d95590b123267f3a103d452707894d3c10f4769
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-09-25 02:28:14 +0100

    examples: Test more 2D sprite sizes in example
    The previous test only showed a couple of sizes, and the texture names
    weren't very descriptive. The new test shows more sizes, and the texture
    names specify their size in pixels.

--- examples/2d_system/quad_texture_sizes/assets.sh
+++ examples/2d_system/quad_texture_sizes/assets.sh
@@ -7,19 +7,25 @@ IMG2DS=$TOOLS/img2ds/img2ds.py
 mkdir -p data
 
 python3 $IMG2DS \
-    --input assets/big.png \
-    --name big \
+    --input assets/s8.png \
+    --name s8 \
     --output data \
-    --format A1RGB5
+    --format PAL16
 
 python3 $IMG2DS \
-    --input assets/medium.png \
-    --name medium \
+    --input assets/s16.png \
+    --name s16 \
     --output data \
-    --format A1RGB5
+    --format PAL16
 
 python3 $IMG2DS \
-    --input assets/small.png \
-    --name small \
+    --input assets/s64.png \
+    --name s64 \
     --output data \
-    --format A1RGB5
+    --format PAL16
+
+python3 $IMG2DS \
+    --input assets/s256.png \
+    --name s256 \
+    --output data \
+    --format PAL16
--- examples/2d_system/quad_texture_sizes/source/main.c
+++ examples/2d_system/quad_texture_sizes/source/main.c
@@ -13,30 +13,56 @@
 
 #include <NEMain.h>
 
-#include "big_tex_bin.h"
-#include "medium_tex_bin.h"
-#include "small_tex_bin.h"
+#include "s8_tex_bin.h"
+#include "s8_pal_bin.h"
+#include "s16_tex_bin.h"
+#include "s16_pal_bin.h"
+#include "s64_tex_bin.h"
+#include "s64_pal_bin.h"
+#include "s256_tex_bin.h"
+#include "s256_pal_bin.h"
 
-NE_Material *Material_small, *Material_medium, *Material_big;
+NE_Material *Material_s8, *Material_s16, *Material_s64, *Material_s256;
+NE_Palette *Palette_s8, *Palette_s16, *Palette_s64, *Palette_s256;
 
 void Draw3DScene(void)
 {
+    NE_ClearColorSet(NE_DarkGray, 31, 63);
+
     NE_2DViewInit();
-
-    // Texture scaled from 8x8 to 32x32
-    NE_2DDrawTexturedQuad(150, 10,
-                          150 + 32, 10 + 32,
-                          0, Material_small);
-
-    // Texture scaled from 16x16 to 64x64
-    NE_2DDrawTexturedQuad(150, 100,
-                          150 + 64, 100 + 64,
-                          1, Material_medium);
 
     // Texture scaled from 64x64 to 128x128
     NE_2DDrawTexturedQuad(10, 10,
                           10 + 128, 10 + 128,
-                          2, Material_big);
+                          2, Material_s64);
+
+    // Texture scaled from 8x8 to 32x32
+    NE_2DDrawTexturedQuad(150, 10,
+                          150 + 32, 10 + 32,
+                          0, Material_s8);
+
+    // Texture not scaled
+    NE_2DDrawTexturedQuad(160, 50,
+                          160 + 64, 50 + 64,
+                          10, Material_s64);
+
+
+    // Texture scaled from 16x16 to 64x64
+    NE_2DDrawTexturedQuad(150, 120,
+                          150 + 64, 120 + 64,
+                          1, Material_s16);
+}
+
+void Draw3DScene2(void)
+{
+    NE_ClearColorSet(NE_Magenta, 31, 63);
+
+    NE_2DViewInit();
+
+    // Texture not scaled
+    NE_2DDrawTexturedQuad(-10, -70,
+                          -10 + 256, -70 + 256,
+                          3, Material_s256);
 }
 
 int main(void)
@@ -45,26 +71,46 @@ int main(void)
     irqSet(IRQ_VBLANK, NE_VBLFunc);
     irqSet(IRQ_HBLANK, NE_HBLFunc);
 
-    NE_Init3D();
+    NE_InitDual3D();
 
-    Material_small = NE_MaterialCreate();
-    Material_medium = NE_MaterialCreate();
-    Material_big = NE_MaterialCreate();
+    Material_s8 = NE_MaterialCreate();
+    Material_s16 = NE_MaterialCreate();
+    Material_s64 = NE_MaterialCreate();
+    Material_s256 = NE_MaterialCreate();
 
-    NE_MaterialTexLoad(Material_small, NE_A1RGB5, 8, 8, NE_TEXGEN_TEXCOORD,
-                       (void *)small_tex_bin);
-    NE_MaterialTexLoad(Material_medium, NE_A1RGB5, 16, 16, NE_TEXGEN_TEXCOORD,
-                       (void *)medium_tex_bin);
-    NE_MaterialTexLoad(Material_big, NE_A1RGB5, 64, 64, NE_TEXGEN_TEXCOORD,
-                       (void *)big_tex_bin);
+    Palette_s8 = NE_PaletteCreate();
+    Palette_s16 = NE_PaletteCreate();
+    Palette_s64 = NE_PaletteCreate();
+    Palette_s256 = NE_PaletteCreate();
 
-    NE_ClearColorSet(NE_DarkGray, 31, 63);
+    NE_MaterialTexLoad(Material_s8, NE_PAL16, 8, 8,
+                       NE_TEXGEN_TEXCOORD | NE_TEXTURE_WRAP_S | NE_TEXTURE_WRAP_T,
+                       (void *)s8_tex_bin);
+    NE_MaterialTexLoad(Material_s16, NE_PAL16, 16, 16,
+                       NE_TEXGEN_TEXCOORD | NE_TEXTURE_WRAP_S | NE_TEXTURE_WRAP_T,
+                       (void *)s16_tex_bin);
+    NE_MaterialTexLoad(Material_s64, NE_PAL16, 64, 64,
+                       NE_TEXGEN_TEXCOORD | NE_TEXTURE_WRAP_S | NE_TEXTURE_WRAP_T,
+                       (void *)s64_tex_bin);
+    NE_MaterialTexLoad(Material_s256, NE_PAL16, 256, 256,
+                       NE_TEXGEN_TEXCOORD | NE_TEXTURE_WRAP_S | NE_TEXTURE_WRAP_T,
+                       (void *)s256_tex_bin);
+
+    NE_PaletteLoad(Palette_s8, (u16 *)s8_pal_bin, 16, NE_PAL16);
+    NE_PaletteLoad(Palette_s16, (u16 *)s16_pal_bin, 16, NE_PAL16);
+    NE_PaletteLoad(Palette_s64, (u16 *)s64_pal_bin, 16, NE_PAL16);
+    NE_PaletteLoad(Palette_s256, (u16 *)s256_pal_bin, 16, NE_PAL16);
+
+    NE_MaterialSetPalette(Material_s8, Palette_s8);
+    NE_MaterialSetPalette(Material_s16, Palette_s16);
+    NE_MaterialSetPalette(Material_s64, Palette_s64);
+    NE_MaterialSetPalette(Material_s256, Palette_s256);
 
     while (1)
     {
         NE_WaitForVBL(0);
 
-        NE_Process(Draw3DScene);
+        NE_ProcessDual(Draw3DScene, Draw3DScene2);
     }
 
     return 0;
