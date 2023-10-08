b66bf16f6f4db2b40c1c53c79fbbf9a3fd593fb9
Antonio Niño Díaz <antonio_nd@outlook.com>
2023-10-08 23:35:09 +0100

    examples: Update dual 3D template to use safe dual 3D

--- examples/templates/3d_dual_screen/source/main.c
+++ examples/templates/3d_dual_screen/source/main.c
@@ -4,6 +4,11 @@
 //
 // This file is part of Nitro Engine
 
+// Note: You can also use NE_Init3D() and NE_ProcessDual(), but it is
+// discouraged. You'll need to ensure that your game never drops frames. If it
+// drops one frame, the image of one screen will be shown in both screens until
+// a new frame is generated (screens are drawn in alternate frames).
+
 #include <NEMain.h>
 
 void Draw3DScene(void)
@@ -22,7 +27,7 @@ int main(void)
     irqSet(IRQ_VBLANK, NE_VBLFunc);
     irqSet(IRQ_VBLANK, NE_HBLFunc);
 
-    NE_InitDual3D();
+    NE_InitSafeDual3D();
 
     while (1)
     {
@@ -32,7 +37,7 @@ int main(void)
 
         // Your code goes here
 
-        NE_ProcessDual(Draw3DScene, Draw3DScene2);
+        NE_ProcessSafeDual3D(Draw3DScene, Draw3DScene2);
     }
 
     return 0;
